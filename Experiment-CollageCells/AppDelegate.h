f683bce52d7ca5e734b15ee351dd632924f65c2b
Travis Jeffery <travisjeffery@gmail.com>
2012-12-03 21:26:57 -0500

    Initial Commit

---
+++ Experiment-CollageCells/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Experiment-CollageCells
+//
+//  Created by Travis Jeffery on 12/3/12.
+//  Copyright (c) 2012 Travis Jeffery. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Experiment-CollageCells/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Experiment-CollageCells
+//
+//  Created by Travis Jeffery on 12/3/12.
+//  Copyright (c) 2012 Travis Jeffery. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
